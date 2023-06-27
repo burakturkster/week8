7e7da5aeb13e60cc12630eed511641a6b03b12e5
Burak Turk <burak.turkster@gmail.com>
2023-06-27 16:39:38 +0300

    Dinamik sonuç üreten şekilde yeniden kodlandı.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react"
 
 function App() {
-    const [bfiyat, bfiyatGuncelle] = useState("")
-    const [adet, adetGuncelle] = useState("")
-    const [kdv, kdvGuncelle] = useState("")
-    const [sonuc, sonucGuncelle] = useState(0)
+    const [bfiyat, bfiyatGuncelle] = useState(0)
+    const [adet, adetGuncelle] = useState(0)
+    const [kdv, kdvGuncelle] = useState(0)
 
     function formGonderim(olay) {
         olay.preventDefault()
-
-        let tutar = parseInt(bfiyat) * parseInt(adet)
-        tutar = tutar + (tutar / 100) * parseInt(kdv)
-        
-        sonucGuncelle(tutar)
     }
 
 
 console.log("App çalıştı")
+
+
+let tutar = parseInt(bfiyat) * parseInt(adet)
+tutar = tutar + (tutar / 100) * parseInt(kdv)
 
 return (
     <>
@@ -24,10 +22,9 @@ return (
             <input type="text" placeholder="Birim Fiyat" onChange={olay => bfiyatGuncelle(olay.target.value)} />
             <input type="text" placeholder="Adet" onChange={olay => adetGuncelle(olay.target.value)} />
             <input type="text" placeholder="KDV % ör:18" onChange={olay => kdvGuncelle(olay.target.value)} />
-            <button type="submit">Hesapla</button>
         </form>
 
-        <p>{sonuc}</p>
+        <p>{tutar}TL</p>
     </>
 )
 }
